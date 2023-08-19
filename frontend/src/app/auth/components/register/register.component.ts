7e71d6e5fd49f42ffb40891daf8f58162bbc6f58
Ye Lwin Htay <yelwin.r.k@gmail.com>
2023-08-18 20:12:42 -0700

    confirm password implemented

--- frontend/src/app/auth/components/register/register.component.ts
+++ frontend/src/app/auth/components/register/register.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
+  AbstractControl,
   FormBuilder,
+  FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+  ValidationErrors
 } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
@@ -17,19 +22,38 @@ import { AuthService } from '../../services/auth.service';
 })
 export class RegisterComponent {
   public registrationForm!: FormGroup;
+  public hasSubmitted:boolean = false
 
   constructor(
     private readonly _fb: FormBuilder,
     private readonly authService: AuthService
   ) {
     this.registrationForm = this._fb.group({
-      username: '',
-      email: '',
-      password: '',
-    });
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+      confirmPassword: new FormControl(null)
+    },
+    {
+      validators: this.matchPassword
+    }
+    );
+  }
+
+  private matchPassword: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    let password = control.get('password')
+    let confirmPassword = control.get('confirmPassword')
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      return {
+        passwordmatcherror: true
+      }
+    }
+    return null
   }
 
   onRegistration() {
+    delete this.registrationForm.value.confirmPassword
+    this.hasSubmitted = true
     this.authService.register(this.registrationForm.value).subscribe();
   }
 }
