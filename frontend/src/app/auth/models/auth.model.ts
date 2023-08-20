312c731b7ac516660277879ba81e2ffe72b62e12
khant <khantmin.htoo@frontiir.net>
2023-08-20 02:34:46 -0700

    logic completed navbar and loginPage

--- frontend/src/app/auth/models/auth.model.ts
+++ frontend/src/app/auth/models/auth.model.ts
@@ -1,3 +1,28 @@
+import { computed, signal } from '@angular/core';
+
 export type MCAuth = {
-    accessToken: string,
+  accessToken: string | null;
+  isLoggedIn: boolean;
+};
+
+export class MAuth {
+  private auth: MCAuth = {
+    accessToken: null,
+    isLoggedIn: false,
+  };
+
+  //   Retrieve the latest auth info
+  getAuth() {
+    return this.auth;
+  }
+
+  //   Set the auth info
+  setAuth(auth: MCAuth) {
+    this.auth = auth;
+  }
+
+  //   Update the auth info
+  updateAuth(data: any) {
+    this.auth = {...this.auth, ...data}
+  }
 }
--- frontend/src/app/core/components/navbar/navbar.component.ts
+++ frontend/src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,17 +11,21 @@ import { Router, RouterModule } from '@angular/router';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  // Connect this is Service -- create both login and logout in the service as well
-  public isLoggedIn = false;
+  public isLoggedIn = computed(() => this.authService.loginSignal());
 
-  constructor(private readonly _router: Router) {}
+  constructor(
+    private readonly _router: Router,
+    private readonly authService: AuthService
+  ) {}
 
+  // Redirect to login page from Navbar
   login() {
-    this.isLoggedIn = true;
+    this._router.navigate(['auth/login']);
   }
 
+  // Redirect to default route from Navbar
   logout() {
+    this.authService.logout();
     this._router.navigate(['/']);
-    this.isLoggedIn = false;
   }
 }
