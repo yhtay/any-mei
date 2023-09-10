db8d7accd437e4022d9eb9fcb0d456de8eedbf4d
khant <khantmin.htoo@frontiir.net>
2023-09-09 23:51:36 -0700

    small fix on nodemailer singleRSP

--- backend/smtp(nodemailer)/nodemailerSMTP.js
+++ backend/smtp(nodemailer)/nodemailerSMTP.js
@@ -1,17 +1,25 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-// Configure Nodemailer to send the email
+// Configure Nodemailer to send the email (sender)
 exports.transporter = () => {
   return nodemailer.createTransport({
     // Your email service configuration here
     service: "Gmail",
     auth: {
-      user: "testkun54@gmail.com",
-      pass: "qnexsctgbojiuhcb",
+      user: process.env.SMTP_USER,
+      pass: proces.env.SMTP_APP_PASS,
     },
   });
 };
 
+/**
+ * Creating a default mailOptions that will send the email subject to the receiver
+ * @param {*} email Email of the receivers
+ * @param {*} subject Subject of the mail
+ * @param {*} message Message of the mail
+ * @returns Returns the format of the how the mailOptions should be
+ */
 exports.mailOptions = (email, subject, message) => {
   return {
     from: "testkun54@gmail.com",
@@ -21,6 +29,12 @@ exports.mailOptions = (email, subject, message) => {
   };
 };
 
+/**
+ * Configuration to send actually email to the receiver
+ * @param {*} sender Transporter functions configuration
+ * @param {*} mailOptions Mail Options configuration
+ * @returns 
+ */
 exports.sendMail = (sender, mailOptions) => {
   return sender.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -29,4 +43,4 @@ exports.sendMail = (sender, mailOptions) => {
       console.log("Email sent: " + info.response);
     }
   });
-}
+};
